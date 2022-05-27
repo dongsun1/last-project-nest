295d9e07e664297f2eaad2a0ffb9e638e9ab43fa
HYUNSEUNG91 <lhs91001@naver.com>
2022-05-27 21:39:32 +0900

    인증서 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/.well-known/pki-validation/010E487D5E54DDAB56FEEBAF2F4EAB37.txt')
+  wellknown(@Res() res : any){
+    res.sendFile(__dirname + '/.well-known/pki-validation/010E487D5E54DDAB56FEEBAF2F4EAB37.txt');
+  }
 }
+
