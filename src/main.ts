6da9c19a3793c1f3a2050f8273a3355c4f2abb0a
HYUNSEUNG91 <lhs91001@naver.com>
2022-05-30 11:24:30 +0900

    Nest JS Https 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
+import *as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+  const httpsOptions = {
+    key : fs.readFileSync("./private.key", "utf-8"),
+    cert : fs.readFileSync("./certificate.crt", "utf-8"),
+    ca : fs.readFileSync("./ca_bundle.crt", "utf-8")
+  };
+  const server = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+  await app.init();
+  
+  http.createServer(server).listen(80);
+  https.createServer(httpsOptions, server).listen(443);
+};
+
 bootstrap();
